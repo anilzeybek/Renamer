2d9720301155161e8ca138da25fb1a277add3a84
anil zeybek <anil.zeybek98@gmail.com>
2019-12-08 20:24:48 +0300

    add changeable extensions add caution add directory specification

--- renamer.py
+++ renamer.py
@@ -1,13 +1,15 @@
-from os import listdir
-from os import getcwd
-from os import rename
+import os
 from os.path import isfile, join
-import sys
+import argparse
+
+extensions_list = ['txt', 'png', 'jpg']
+header = ''
+skip = False
+directory = ''
 
 
-def pdf_or_txt(f_name):
-    extensions_list = ['.pdf', '.txt', '.png', '.jpg']
-    extension = f_name[-4:]
+def extension_compatible(f_name):
+    extension = f_name.split('.')[-1]
 
     if extension in extensions_list:
         return True
@@ -15,20 +17,64 @@ def pdf_or_txt(f_name):
         return False
 
 
-if __name__ == '__main__':
-    header = ''
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '-n':
-            header = sys.argv[2] + '-'
+def parse_args():
+    global header, skip, directory
+
+    parser = argparse.ArgumentParser(description='Rename files in order')
+    parser.add_argument('-n', '--name', type=str, help='Headers name for files to put before numbers')
+    parser.add_argument('-e', '--extensions', type=str, help='Headers for files to put before numbers')
+    parser.add_argument('-y', '--yes', action='store_const', const=True, help="Don't ask for inputting y")
+    parser.add_argument('directory', type=str, help='Directory location of the files that will renamed')
+    args = parser.parse_args()
+
+    header = f'{args.name}-' if args.name else ''
+
+    if args.extensions:
+        mutate_extension_list(args.extensions)
+
+    skip = args.yes if args.yes else False
+
+    directory = args.directory
+
+
+def mutate_extension_list(new_extensions: str):
+    global extensions_list
+    extensions_list = new_extensions.split()
+
+
+def rename_files(files):
+    for i in range(len(files)):
+        ext = files[i].split('.')[-1]
+        os.rename(files[i], f'{header}{i + 1}.{ext}')
+
+
+def main():
+    parse_args()
+
+    if directory == '.':
+        files = [f for f in os.listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
+    else:
+        files = [f for f in os.listdir(directory) if isfile(join(os.getcwd(), f))]
+
+    necessary_files = sorted(filter(extension_compatible, files))
+
+    if skip:
+        rename_files(necessary_files)
+        print("DONE!")
+    else:
+        print('This will rename every file with extensions:')
+        for i in range(len(extensions_list)):
+            print(f'{i}- {extensions_list[i]}')
+
+        print('Do you want to continue? y/n')
+        selection = input()
+
+        if selection == 'y':
+            rename_files(necessary_files)
+            print("DONE!")
         else:
-            print("Usage: -n HEADER_NAME")
-            sys.exit(0)
+            print('Cancelled')
 
-    files = [f for f in listdir(getcwd()) if isfile(join(getcwd(), f))]
-    necessary_files = sorted(filter(pdf_or_txt, files))
 
-    for i in range(len(necessary_files)):
-        ext = necessary_files[i][-4:]
-        rename(necessary_files[i], f'{header}{i + 1}{ext}')
-
-    print("DONE!:")
+if __name__ == '__main__':
+    main()
